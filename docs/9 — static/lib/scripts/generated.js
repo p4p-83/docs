0abf2fde2ab7db2c24a552cd0650a448812a4ba2
James <james@jamesnzl.xyz>
2023-10-11 14:53:18 +1300

    docs(obsidian): :memo: regenerate static docs

---
+++ docs/9 — static/lib/scripts/generated.js
@@ -0,0 +1,1 @@
+let nodes={paths:["!-part-iv-project.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["! Part IV Project"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
